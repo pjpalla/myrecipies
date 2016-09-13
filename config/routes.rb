b693f10ef5bb37f131e3030f0b97872c99399a84
pj <palla.pg@gmail.com>
2016-09-13 08:44:23 +0000

    create new and edit form and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   
   root 'pages#home'
-  get '/home', to: 'pages#home' 
+  get '/home', to: 'pages#home'
+  
+  # get '/recipes', to: 'recipes#index'
+  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  # post '/recipes', to: 'recipes#create'
+  # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  # patch 'recipes/:id', to: 'recipe#update'
+  # get 'recipes/:id', to: 'recipe#show', as: 'recipe'
+  # delete 'recipes/:id', to: 'recipes#destroy'
+  
+  resources :recipes
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
